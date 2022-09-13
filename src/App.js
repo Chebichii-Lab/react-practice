2a44f185e1b28573ec999dd05469606f2ec15ab2
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2022-09-13 17:52:28 +0300

    use state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Employee from './components/Employee';
+import {useState} from 'react';
 
 function App() {
+  const [role, setRole] = useState('dev');
   const showEmployees = true;
   return (
     <div className="App">
       {showEmployees ? 
       <>
+      <input type = 'text' onChange = {(e) => {
+        console.log(e.target.value)
+        setRole(e.target.value);
+      }}/>
       <Employee name = "Natasha" role = "Intern"/>
-      <Employee name = "Jaffari"/>
+      <Employee name = "Jaffari" role ={role}/>
       <Employee name = "Zane"/>
       </>
       :
