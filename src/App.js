c68d7f8e4de634a5f241314d6b751f7aef9a3dce
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2022-10-23 12:24:57 +0300

    mapping through State Array

--- src/App.js
+++ src/App.js
@@ -2,9 +2,42 @@ import logo from "./logo.svg";
 import "./index.css";
 import Employee from "./components/Employee";
 import { useState } from "react";
+import {v4 as uuidv4} from 'uuid';
 
 function App() {
   const [role, setRole] = useState("dev");
+  const [employees, setEmployee] = useState([
+    {
+      name: "Natasha",
+      role: "Developer",
+      img: "https://images.pexels.com/photos/4355346/pexels-photo-4355346.jpeg",
+    },
+    {
+      name: "Maribel",
+      role: "Manager",
+      img: "https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg",
+    },
+    {
+      name: "Louisa",
+      role: "Director of Eng",
+      img: "https://images.pexels.com/photos/2728264/pexels-photo-2728264.jpeg",
+    },
+    {
+      name: "Abuela",
+      role: "Software Engineer",
+      img: "https://images.pexels.com/photos/3438086/pexels-photo-3438086.jpeg",
+    },
+    {
+      name: "Pepa",
+      role: "Devops",
+      img: "https://images.pexels.com/photos/1840608/pexels-photo-1840608.jpeg",
+    },
+    {
+      name: "Bruno",
+      role: "Team Lead",
+      img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg",
+    },
+  ]);
   const showEmployees = true;
   return (
     <div className="App">
@@ -17,13 +50,17 @@ function App() {
               setRole(e.target.value);
             }}
           />
-          <div className = "flex flex-wrap justify-center">
-            <Employee name="Natasha" role="Intern" img = "https://media.istockphoto.com/photos/man-in-cosplaying-thor-isolated-on-white-studio-background-picture-id1147508579?b=1&k=20&m=1147508579&s=612x612&w=0&h=ThteD02H2UsCYKSBz2aqs_IDuEwJbrf6vFd8pDMGHDU="/>
-            <Employee name="Jaffari" role={role} img = "https://media.istockphoto.com/photos/man-in-cosplaying-thor-isolated-on-white-studio-background-picture-id1147508579?b=1&k=20&m=1147508579&s=612x612&w=0&h=ThteD02H2UsCYKSBz2aqs_IDuEwJbrf6vFd8pDMGHDU=" />
-            <Employee name="Zane" img = "https://media.istockphoto.com/photos/man-in-cosplaying-thor-isolated-on-white-studio-background-picture-id1147508579?b=1&k=20&m=1147508579&s=612x612&w=0&h=ThteD02H2UsCYKSBz2aqs_IDuEwJbrf6vFd8pDMGHDU="/>
-            <Employee name="Natasha" role="Intern" img = "https://media.istockphoto.com/photos/man-in-cosplaying-thor-isolated-on-white-studio-background-picture-id1147508579?b=1&k=20&m=1147508579&s=612x612&w=0&h=ThteD02H2UsCYKSBz2aqs_IDuEwJbrf6vFd8pDMGHDU="/>
-            <Employee name="Jaffari" role={role} img = "https://media.istockphoto.com/photos/man-in-cosplaying-thor-isolated-on-white-studio-background-picture-id1147508579?b=1&k=20&m=1147508579&s=612x612&w=0&h=ThteD02H2UsCYKSBz2aqs_IDuEwJbrf6vFd8pDMGHDU="/>
-            <Employee name="Zane" img = "https://media.istockphoto.com/photos/man-in-cosplaying-thor-isolated-on-white-studio-background-picture-id1147508579?b=1&k=20&m=1147508579&s=612x612&w=0&h=ThteD02H2UsCYKSBz2aqs_IDuEwJbrf6vFd8pDMGHDU="/>
+          <div className="flex flex-wrap justify-center">
+            {employees.map((employee) => {
+              return (
+                <Employee
+                  key={uuidv4()}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                />
+              );
+            })}
           </div>
         </>
       ) : (
